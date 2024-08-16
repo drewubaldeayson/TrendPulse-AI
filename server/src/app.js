091a4db92bdc4a3485a9982647e0036cbf44cbac
Keanu Reaño <reanokeanu@gmail.com>
2024-08-16 13:49:09 +0800

    Add initial server setup in app.js Created a new file app.js in the server/src directory with basic express server setup, defining a route that returns "Hello World" and listens on a specified port.

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cookieparser = require("cookie-parser");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieparser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
