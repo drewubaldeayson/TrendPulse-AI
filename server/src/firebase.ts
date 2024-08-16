c0de07730e4733f0d476d9f5425a3226560b250c
Keanu Reaño <reanokeanu@gmail.com>
2024-08-16 15:14:58 +0800

    Update server package.json to point to the correct main file in the dist directory and modify the start script to run the correct file. Add firebase.ts file with Firebase configuration details. Update index.ts to use Firebase authentication for listing users. Include references for using Cloud Firestore and Firebase authentication with Express.js.

---
+++ server/src/firebase.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import account from "../serviceAccount.json";
+import admin from "firebase-admin";
+
+dotenv.config();
+
+const {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} = process.env;
+
+export const serviceAccount = account as admin.ServiceAccount;
+
+export const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,33 @@
 import express, { Request, Response } from "express";
 import admin from "firebase-admin";
-import dotenv from "dotenv";
-import serviceAccount from "../serviceAccount.json";
-dotenv.config();
+import { serviceAccount } from "./firebase";
 
 const app = express();
 
 app.use(express());
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+  admin
+    .auth()
+    .listUsers()
+    .then((users) => {
+      const usersData = users.users.map((user) => {
+        return {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+        };
+      });
+      res.json(usersData);
+    })
+    .catch((error) => {
+      console.error("Error listing users:", error);
+      res.status(500).json({ error: "Failed to retrieve users" });
+    });
 });
 
 const PORT = process.env.PORT || 8080;
@@ -21,3 +35,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// REFERENCES: https://medium.com/@victoriahjeon/using-cloud-firestore-and-firebase-authentication-with-express-js-87fe99e16ead
+// REFERENCES: https://medium.com/@swapkumbhar31/simplifying-user-authentication-with-firebase-node-js-express-and-typescript-e52b3fc0a780
