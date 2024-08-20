ceb77bcbe529a4030882a854bbf7479c3c7ba19c
Keanu Reaño <reanokeanu@gmail.com>
2024-08-20 13:29:38 +0800

    Refactor RedirectLayout component to handle authentication logic asynchronously. Update to store user token in sessionStorage if user is authenticated.

--- client/src/firebase/redirectLayout.tsx
+++ client/src/firebase/redirectLayout.tsx
@@ -15,15 +15,21 @@ export default function RedirectLayout({
     typeof window !== "undefined" ? sessionStorage.getItem("user") : null;
 
   useEffect(() => {
-    const isOnAuthPage = pathname === "/sign-in" || pathname === "/sign-up";
-    const isAuthenticated = user || userSession;
+    const authenticate = async () => {
+      const isOnAuthPage = pathname === "/sign-in" || pathname === "/sign-up";
+      const isAuthenticated = user || userSession;
 
-    if (isAuthenticated && isOnAuthPage) {
-      router.push("/");
-    } else if (!isAuthenticated && !isOnAuthPage) {
-      router.push("/sign-in");
-    }
-  }, [user, pathname, userSession]);
+      if (!userSession && user) {
+        sessionStorage.setItem("user", await user.getIdToken());
+      }
+      if (isAuthenticated && isOnAuthPage) {
+        router.push("/");
+      } else if (!isAuthenticated && !isOnAuthPage) {
+        router.push("/sign-in");
+      }
+    };
+    authenticate();
+  }, [user, userSession]);
 
   if (loading) {
     return <p>Loading...</p>;
