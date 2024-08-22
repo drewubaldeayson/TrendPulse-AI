c5643524e6167f868910078996eadfed058ebb83
Keanu Reaño <reanokeanu@gmail.com>
2024-08-22 18:57:12 +0800

    Update Navbar component to replace Link with a tag for better accessibility and SEO.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,13 @@
-import Link from "next/link";
 import LogoutButton from "./LogoutButton";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
     <div className="px-2 sticky top-0 h-16 border bg-primary-foreground flex items-center justify-between">
-      <Button variant="secondary">
-        <Link href="/">
+      <Button variant="ghost">
+        <a href="/">
           <span className="font-bold">TrendPulse-AI</span>
-        </Link>
+        </a>
       </Button>
       <LogoutButton />
     </div>
