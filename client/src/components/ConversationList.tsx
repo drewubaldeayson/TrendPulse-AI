73694f7126f5249bfdf8ec85229c910c7652bdb2
Keanu Reaño <reanokeanu@gmail.com>
2024-08-23 09:20:22 +0800

    Add react-icons package to dependencies, update ConversationList component to include a delete button with trash can icon, and adjust styling in WrapperSidebar component in the client codebase.

--- client/src/components/ConversationList.tsx
+++ client/src/components/ConversationList.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { Dispatch, SetStateAction } from "react";
 import { Button } from "./ui/button";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 export interface Conversation {
   id: string;
@@ -21,19 +22,29 @@ export default function ConversationList({
   return (
     <>
       {conversations.map((convo) => (
-        <Button
-          variant="ghost"
-          key={convo.id}
-          onClick={() => setConversation(convo)}
-          className={clsx(
-            "text-xs md:text-sm px-0 w-full truncate text-start block",
-            {
-              underline: convo.id === conversation?.id,
-            }
-          )}
-        >
-          {convo.title}
-        </Button>
+        <div className="relative group">
+          <Button
+            variant="ghost"
+            key={convo.id}
+            onClick={() => setConversation(convo)}
+            className={clsx(
+              "px-2 md:px-4 text-xs md:text-sm w-full truncate text-start block",
+              {
+                underline: convo.id === conversation?.id,
+              }
+            )}
+          >
+            {convo.title}
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-0 -translate-y-1/2 opacity-0 top-1/2 group-hover:opacity-100 bg-primary-foreground"
+            onClick={() => console.log(convo.id)}
+          >
+            <FaRegTrashCan />
+          </Button>
+        </div>
       ))}
     </>
   );
--- client/src/components/WrapperSidebar.tsx
+++ client/src/components/WrapperSidebar.tsx
@@ -4,7 +4,7 @@ export default function WrapperSidebar({
   children: React.ReactNode;
 }) {
   return (
-    <div className="px-2 md:px-4 py-4 border w-32 md:w-64 bg-primary-foreground">
+    <div className="py-4 border w-32 md:w-64 bg-primary-foreground">
       {children}
     </div>
   );
