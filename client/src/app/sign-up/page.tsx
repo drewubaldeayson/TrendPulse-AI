95fcbc206afce495b61175dbb27aecfd906e6805
Keanu Reaño <reanokeanu@gmail.com>
2024-08-23 14:25:40 +0800

    Add new dependencies to package.json and pnpm-lock.yaml
    Updated client/package.json to add "@hookform/resolvers": "^3.9.0" and "@radix-ui/react-label": "^2.1.0" dependencies. Also included "react-hook-form": "^7.52.2" and "zod": "^3.23.8" in the dependencies list.
    
    Updated client/pnpm-lock.yaml to reflect the added dependencies with their versions and peer dependencies.

--- client/src/app/sign-up/page.tsx
+++ client/src/app/sign-up/page.tsx
@@ -3,34 +3,115 @@ import { useState } from "react";
 import { auth } from "@/firebase/config";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z
+  .object({
+    email: z
+      .string()
+      .email("Invalid email address")
+      .nonempty("Email is required"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // Path to indicate the error location
+  });
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [createUserWithEmailAndPassword, user, _loading, error] =
+  const [createUserWithEmailAndPassword, _user, _loading, error] =
     useCreateUserWithEmailAndPassword(auth);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const signUpHandler = (data: z.infer<typeof formSchema>) => {
+    createUserWithEmailAndPassword(data.email, data.password);
+  };
+
   return (
-    <div>
-      <input
-        type="email"
-        value={email}
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={() => createUserWithEmailAndPassword(email, password)}>
-        Sign Up
-      </button>
-      {error && <p>{error.message}</p>}
-      <Link href="/sign-in" className="underline">
-        <p>Already have an account?</p>
-      </Link>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-primary-foreground">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(signUpHandler)}
+          className="border p-8 rounded bg-white prose min-w-[32rem] space-y-8"
+        >
+          <h1 className="text-center">TrendPulse-AI</h1>
+          <h2 className="m-0">Sign Up</h2>
+          {/* Email Field */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="relative">
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage className="absolute right-0 top-0" />
+              </FormItem>
+            )}
+          />
+
+          {/* Password Field */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="relative">
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage className="absolute right-0 top-0" />
+              </FormItem>
+            )}
+          />
+
+          {/* Confirm Password Field */}
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem className="relative">
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Confirm Password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="absolute right-0 top-0" />
+              </FormItem>
+            )}
+          />
+
+          {/* Submit Button */}
+          <Button type="submit">Sign Up</Button>
+          {error && <p className="text-destructive">{error.message}</p>}
+          <Link href="/sign-in" className="underline">
+            <p>Already have an account?</p>
+          </Link>
+        </form>
+      </Form>
+    </main>
   );
 }
