6f3dbfee88aa1a86eab4c622cd703a5a4851817d
Keanu Reaño <reanokeanu@gmail.com>
2024-08-28 08:07:30 +0800

    Add useFormContext hook to MessageHeader component and update onClick event to use handleOnClick function which will set value and focus.

--- client/src/components/MessageHeader.tsx
+++ client/src/components/MessageHeader.tsx
@@ -1,11 +1,20 @@
+"use client";
 import { auth } from "@/firebase/config";
 import { useAuthState } from "react-firebase-hooks/auth";
 import templates from "@/lib/templates.json";
 import { Category } from "@/app/templates/page";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { useFormContext } from "react-hook-form";
 
 export default function MessageHeader() {
   const [user] = useAuthState(auth);
+  const { setValue, setFocus } = useFormContext();
+
+  const handleOnClick = (message: string) => {
+    setValue("message", message);
+    setFocus("message");
+  };
+
   return (
     <section>
       <h2 className="leading-normal md:text-4xl">
@@ -16,7 +25,7 @@ export default function MessageHeader() {
         {templates.map((category: Category) => (
           <Card
             className="transition cursor-pointer hover:bg-primary-foreground"
-            onClick={() => console.log(category)}
+            onClick={() => handleOnClick(category.templates[0].message)}
           >
             <CardHeader>
               <CardTitle className="text-xl">{category.title}</CardTitle>
