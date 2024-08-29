e76b561caabd544a2b07c290aef02b5e8fd3c722
Keanu Reaño <reanokeanu@gmail.com>
2024-08-29 10:10:59 +0800

    Added a hero section with a message and button in the page.tsx file.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main>
+      <HeroSection />
+    </main>
+  );
+}
+
+function HeroSection() {
+  return (
+    <section className="bg-accent">
+      <div className="container py-32 flex">
+        <div className="w-1/2 prose">
+          <h1 className="font-black text-5xl">
+            Chat smarter <br />
+            with Trendpulse-AI
+          </h1>
+          <p>
+            Trendpulse-AI is a free AI-powered assistant that combines the power
+            of ChatGPT with unique data sources, including the HubSpot CRM, to
+            help supercharge your work.
+          </p>
+          <Link href="/chat">
+            <Button>Start chatting free</Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
