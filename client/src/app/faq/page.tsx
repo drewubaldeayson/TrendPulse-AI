97b15c56e962303cafd478cdb972b5bebea97bef
Keanu Reaño <reanokeanu@gmail.com>
2024-09-09 09:14:56 +0800

    Added "@radix-ui/react-accordion" package to the client's package.json and pnpm-lock.yaml with the version "^1.2.0". Updated FAQ page layout content. Added a new accordion component in a new file. Made changes to tailwind.config.ts to extend styles and themes.

--- client/src/app/faq/page.tsx
+++ client/src/app/faq/page.tsx
@@ -1,9 +1,30 @@
 export default function FAQ() {
   return (
-    <main className="bg-accent min-h-screen">
-      <div className="container py-8">
-        <h1>FAQ</h1>
-      </div>
+    <main>
+      <section className="bg-primary-foreground">
+        <div className="container py-8 prose text-center text-balance lg:px-32">
+          <h1>Trendpulse FAQ</h1>
+          <p>
+            The purpose of these FAQs is to provide you with high-level
+            information about TrendPulse. Your use of TrendPulse is governed by
+            our Terms of Service. We strive to provide accurate information, but
+            please be aware that these FAQs may be updated or revised as our
+            platform evolves.
+          </p>
+        </div>
+      </section>
+      <section className="bg-accent">
+        <div className="container py-8 prose">
+          <h1>FAQs</h1>
+          <p>
+            The purpose of these FAQs is to provide you with high-level
+            information about TrendPulse. Your use of TrendPulse is governed by
+            our Terms of Service. We strive to provide accurate information, but
+            please be aware that these FAQs may be updated or revised as our
+            platform evolves.
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
