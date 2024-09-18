70f52df0a5dfdd2b8d84f03734d3380a2d5055f4
Keanu Reaño <reanokeanu@gmail.com>
2024-09-18 15:36:19 +0800

    Refactor pricing page to dynamically render monthly and yearly plans using map function. Introduce `PlanCard` component to display plan details such as title, price, features, and button label.

--- client/src/app/pricing/page.tsx
+++ client/src/app/pricing/page.tsx
@@ -1,9 +1,80 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import { TabsList } from "@radix-ui/react-tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Pricing() {
+  const monthlyPlans = [
+    {
+      title: "BASIC",
+      price: "$49/mo",
+      billingInfo: "Billed annually",
+      features: [
+        "1 dedicated user account",
+        "Unlimited access to Phlanx Complete",
+        "Complete suite of marketing tools",
+      ],
+      buttonLabel: "Start Free Trial",
+    },
+    {
+      title: "PREMIUM",
+      price: "$49/mo",
+      billingInfo: "Billed annually",
+      features: [
+        "3 dedicated user accounts",
+        "Unlimited access to Phlanx Complete",
+        "Complete suite of marketing tools",
+      ],
+      buttonLabel: "Start Free Trial",
+    },
+    {
+      title: "BUSINESS",
+      price: "$100/mo",
+      billingInfo: "Billed annually",
+      features: [
+        "8 dedicated user accounts",
+        "Unlimited access to Phlanx Complete",
+        "Complete suite of marketing tools",
+      ],
+      buttonLabel: "Start Free Trial",
+    },
+  ];
+
+  const yearlyPlans = [
+    {
+      title: "BASIC",
+      price: "$39/mo",
+      billingInfo: "Billed annually for $468",
+      features: [
+        "1 dedicated user account",
+        "Unlimited access to Phlanx Complete",
+        "Complete suite of marketing tools",
+      ],
+      buttonLabel: "Start Free Trial",
+    },
+    {
+      title: "PREMIUM",
+      price: "$60/mo",
+      billingInfo: "Billed annually for $720",
+      features: [
+        "3 dedicated user accounts",
+        "Unlimited access to Phlanx Complete",
+        "Complete suite of marketing tools",
+      ],
+      buttonLabel: "Start Free Trial",
+    },
+    {
+      title: "BUSINESS",
+      price: "$90/mo",
+      billingInfo: "Billed annually for $1080",
+      features: [
+        "8 dedicated user accounts",
+        "Unlimited access to Phlanx Complete",
+        "Complete suite of marketing tools",
+      ],
+      buttonLabel: "Start Free Trial",
+    },
+  ];
+
   return (
     <main className="bg-accent">
       <div className="container min-h-screen py-32">
@@ -19,92 +90,54 @@ export default function Pricing() {
           </TabsList>
           <TabsContent value="monthly">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <Card className="prose prose-sm">
-                <CardHeader className="flex flex-col items-center">
-                  <h1>BASIC</h1>
-                  <h2>$49/mo</h2>
-                  <i className="opacity-0">Billed annually</i>
-                  <Button>Start Free Trial</Button>
-                </CardHeader>
-                <CardContent>
-                  <p>✔️ 1 dedicated user account</p>
-                  <p>✔️ Unlimited access to Phlanx Complete</p>
-                  <p>✔️ Complete suite of marketing tools</p>
-                </CardContent>
-              </Card>
-              <Card className="prose prose-sm">
-                <CardHeader className="flex flex-col items-center">
-                  <h1>PREMIUM</h1>
-                  <h2>$49/mo</h2>
-                  <i className="opacity-0">Billed annually</i>
-                  <Button className="w-fit">Start Free Trial</Button>
-                </CardHeader>
-                <CardContent>
-                  <p>✔️ 3 dedicated user accounts</p>
-                  <p>✔️ Unlimited access to Phlanx Complete</p>
-                  <p>✔️ Complete suite of marketing tools</p>
-                </CardContent>
-              </Card>
-              <Card className="prose prose-sm">
-                <CardHeader className="flex flex-col items-center">
-                  <h1>BUSINESS</h1>
-                  <h2>$100/mo</h2>
-                  <i className="opacity-0">Billed annually</i>
-                  <Button>Start Free Trial</Button>
-                </CardHeader>
-                <CardContent>
-                  <p>✔️ 8 dedicated user accounts</p>
-                  <p>✔️ Unlimited access to Phlanx Complete</p>
-                  <p>✔️ Complete suite of marketing tools</p>
-                </CardContent>
-              </Card>
+              {monthlyPlans.map((plan, index) => (
+                <PlanCard key={index} {...plan} />
+              ))}
             </div>
           </TabsContent>
           <TabsContent value="yearly">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <Card className="prose prose-sm">
-                <CardHeader className="flex flex-col items-center">
-                  <h1>BASIC</h1>
-                  <h2>$39/mo</h2>
-                  <i>Billed annually for $468</i>
-                  <Button>Start Free Trial</Button>
-                </CardHeader>
-                <CardContent>
-                  <p>✔️ 1 dedicated user account</p>
-                  <p>✔️ Unlimited access to Phlanx Complete</p>
-                  <p>✔️ Complete suite of marketing tools</p>
-                </CardContent>
-              </Card>
-              <Card className="prose prose-sm">
-                <CardHeader className="flex flex-col items-center">
-                  <h1>PREMIUM</h1>
-                  <h2>$60/mo</h2>
-                  <i>Billed annually for $720</i>
-                  <Button>Start Free Trial</Button>
-                </CardHeader>
-                <CardContent>
-                  <p>✔️ 3 dedicated user accounts</p>
-                  <p>✔️ Unlimited access to Phlanx Complete</p>
-                  <p>✔️ Complete suite of marketing tools</p>
-                </CardContent>
-              </Card>
-              <Card className="prose prose-sm">
-                <CardHeader className="flex flex-col items-center">
-                  <h1>BUSINESS</h1>
-                  <h2>$90/mo</h2>
-                  <i>Billed annually for $1080</i>
-                  <Button>Start Free Trial</Button>
-                </CardHeader>
-                <CardContent>
-                  <p>✔️ 8 dedicated user accounts</p>
-                  <p>✔️ Unlimited access to Phlanx Complete</p>
-                  <p>✔️ Complete suite of marketing tools</p>
-                </CardContent>
-              </Card>
+              {yearlyPlans.map((plan, index) => (
+                <PlanCard key={index} {...plan} />
+              ))}
             </div>
           </TabsContent>
         </Tabs>
       </div>
     </main>
+  );
+}
+
+interface PlanCardProps {
+  title: string;
+  price: string;
+  billingInfo: string;
+  features: string[];
+  buttonLabel: string;
+  className?: string;
+}
+
+function PlanCard({
+  title,
+  price,
+  billingInfo,
+  features,
+  buttonLabel,
+  className,
+}: PlanCardProps) {
+  return (
+    <Card className={`prose prose-sm ${className}`}>
+      <CardHeader className="flex flex-col items-center">
+        <h1>{title}</h1>
+        <h2>{price}</h2>
+        <i>{billingInfo}</i>
+        <Button>{buttonLabel}</Button>
+      </CardHeader>
+      <CardContent>
+        {features.map((feature, index) => (
+          <p key={index}>✔️ {feature}</p>
+        ))}
+      </CardContent>
+    </Card>
   );
 }
