1310412d38c0a69e136961026ff1b6693b2600b7
Keanu Reaño <reanokeanu@gmail.com>
2024-09-24 09:41:37 +0800

    Renamed `redirectLayout.tsx` to `RedirectLayout.tsx` and updated the import path in `layout.tsx` to reflect the change.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import RedirectLayout from "@/firebase/RedirectLayout";
+import RedirectLayout from "@/components/RedirectLayout";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import FormProvider from "@/components/FormProvider";
