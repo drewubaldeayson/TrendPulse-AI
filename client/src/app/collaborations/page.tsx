e7671dfbb483a05146ebbc978e0ff783e629c97f
Keanu Reaño <reanokeanu@gmail.com>
2024-10-01 11:38:07 +0800

    Add new features to the CreateCollaborationButton component in the Collaborations page, allowing users to view a list of features available with a subscription. Replace hard-coded features with a dynamic list displayed using a map function. Update the button layout and add a link to the sign-up page for users who are not subscribed.

--- client/src/app/collaborations/page.tsx
+++ client/src/app/collaborations/page.tsx
@@ -31,76 +31,80 @@ function TitleSection() {
 }
 
 function CreateCollaborationButton() {
+  const features = [
+    {
+      title: "✔️ Engagement Calculators",
+      description:
+        "Unlimited engagement searches for Instagram, TikTok, X(Twitter), Facebook, YouTube, and Twitch",
+    },
+    {
+      title: "✔️ Social Media Auditors",
+      description:
+        "Unlimited audits for Instagram, TikTok, and YouTube accounts",
+    },
+    {
+      title: "✔️ Brand Mentions",
+      description:
+        "Unlimited projects and keyword monitoring for the entire web",
+    },
+    {
+      title: "✔️ Influencer Directory",
+      description: "Search and contact over 100,000 influencers",
+    },
+    {
+      title: "✔️ Engagement Calculator Manager",
+      description: "Create informative lists for your social campaigns",
+    },
+    {
+      title: "✔️ Campaign Manager",
+      description: "Manage all your social media campaigns",
+    },
+    {
+      title: "✔️ Competitor Tracker",
+      description:
+        "Compare statistics of your competitors with simplified graphs",
+    },
+    {
+      title: "✔️ Collaborations",
+      description: "Post and respond to other brands and influencers' posts",
+    },
+    {
+      title: "✔️ Contracts",
+      description: "Generate unlimited contracts for your influencer campaigns",
+    },
+    {
+      title: "✔️ Dedicated user accounts",
+      description: "Unlimited access to Phlanx Complete",
+    },
+    { title: "✔️ Free 30 days trial", description: "" },
+  ];
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline">Create Collaboration</Button>
       </DialogTrigger>
       <DialogContent className="max-w-sm md:max-w-xl">
-        <DialogHeader>
+        <DialogHeader className="items-center">
           <DialogTitle>Oops! You need to be subscribed.</DialogTitle>
           <DialogDescription>
             Sign up for 30 days free trial to get full access.
           </DialogDescription>
         </DialogHeader>
-        <Link href="/sign-up">
-          <Button className="w-fit">Sign Up Now</Button>
-        </Link>
+
+        <Button className="mx-16">
+          <Link href="/sign-up">Sign Up Now</Link>
+        </Button>
+
         <div className="prose prose-sm">
-          <p>
-            <b>✔️ Engagement Calculators</b>
-            <br />
-            Unlimited engagement searches for Instagram, TikTok, X(Twitter),
-            Facebook, YouTube, and Twitch
-          </p>
-          <p>
-            <b>✔️ Social Media Auditors</b>
-            <br />
-            Unlimited audits for Instagram, TikTok, and YouTube accounts
-          </p>
-          <p>
-            <b>✔️ Brand Mentions</b>
-            <br />
-            Unlimited projects and keyword monitoring for the entire web
-          </p>
-          <p>
-            <b>✔️ Influencer Directory</b>
-            <br />
-            Search and contact over 100,000 influencers
-          </p>
-          <p>
-            <b>✔️ Engagement Calculator Manager</b>
-            <br />
-            Create informative lists for your social campaigns
-          </p>
-          <p>
-            <b>✔️ Campaign Manager</b>
-            <br />
-            Manage all your social media campaigns
-          </p>
-          <p>
-            <b>✔️ Competitor Tracker</b>
-            <br />
-            Compare statistics of your competitors with simplified graphs
-          </p>
-          <p>
-            <b>✔️ Collaborations</b>
-            <br />
-            Post and respond to other brands and influencers' posts
-          </p>
-          <p>
-            <b>✔️ Contracts</b>
-            <br />
-            Generate unlimited contracts for your influencer campaigns
-          </p>
-          <p>
-            <b>✔️ Dedicated user accounts</b>
-            <br />
-            Unlimited access to Phlanx Complete
-          </p>
-          <p>
-            <b>✔️ Free 30 days trial</b>
-          </p>
+          <p>Other features you'll also get full access to:</p>
+          {features.map((feature, index) => (
+            <p key={index}>
+              <b>{feature.title}</b>
+              <br />
+              {feature.description}
+            </p>
+          ))}
         </div>
       </DialogContent>
     </Dialog>
