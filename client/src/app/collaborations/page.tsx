9d99819619f1350f77b60b545912177275315b81
Keanu Reaño <reanokeanu@gmail.com>
2024-10-01 10:14:57 +0800

    Refactor CollaborationsPage component to include additional styling and structure within main tag.

--- client/src/app/collaborations/page.tsx
+++ client/src/app/collaborations/page.tsx
@@ -1,3 +1,11 @@
 export default function CollaborationsPage() {
-  return <main>Collaborations</main>;
+  return (
+    <main className="bg-accent">
+      <div className="container min-h-screen py-8">
+        <section className="prose">
+          <h2>Collaborations</h2>
+        </section>
+      </div>
+    </main>
+  );
 }
